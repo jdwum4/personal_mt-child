fc71ab9d7aff344b661ded112a6772520021e42a
GH Tech Architect <jdwum4@users.noreply.github.com>
2020-04-16 18:22:52 -0400

    Adding css & function
    adding an additional css style sheet from the parent theme &  deleted duplicated php syntax

--- functions.php
+++ functions.php
@@ -1,6 +1,18 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    
+    $parent_style = 'parent-style'; // This is 'personal-style' for the Personal theme.
  
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', 
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+    wp_enqueue_style( 'child-style', 
+    get_stylesheet_directory_uri() . '/rtl.css',
+    array( $parent_style ),
+    wp_get_theme()->get('Version')
+);
 }
